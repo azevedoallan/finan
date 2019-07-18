d3de9edc8550d6f24eaa1e868974dd621dfa5964
Allan Azevedo <azevedoallan3@gmail.com>
2019-07-17 23:10:11 -0300

    criando plugin

--- src/Application.php
+++ src/Application.php
@@ -1,7 +1,9 @@
 <?php
-
+declare(strict_types=1);
 namespace SONFin;
 
+
+use SONFin\Plugins\PluginInterface;
 
 class Application
 {
@@ -21,13 +23,18 @@ class Application
         return $this->serviceContainer->get($name);
     }
 
-    public function addService(string $name, $service)
+    public function addService(string $name, $service): void
     {
         if (is_callable($service)) {
             $this->serviceContainer->addLazy($name, $service);
         }else{
             $this->serviceContainer->add($name, $service);
         }
+    }
+
+    public function plugin(PluginInterface $plugin): void
+    {
+        $plugin->register($this->serviceContainer);
     }
 
 
---
+++ src/Plugins/PluginInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace SONFin\Plugins;
+
+
+use SONFin\ServiceContainerInterface;
+
+interface PluginInterface
+{
+    public function register(ServiceContainerInterface $container);
+}
