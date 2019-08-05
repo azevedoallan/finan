f60ecb5f5760ae390b86e0eeb20ec59d6f5dbc5b
Allan Azevedo <azevedoallan3@gmail.com>
2019-08-05 00:02:38 -0300

    Edição de categoria de custo

--- public/index.php
+++ public/index.php
@@ -39,11 +39,27 @@ $app
         $view = $app->service('view.renderer');
         return $view->render('category-costs/create.html.twig');
     }, 'category-costs.new')
-    ->post('/category-costs/store', function(ServerRequestInterface $request) use ($app) {
+    ->post('/category-costs/store', function (ServerRequestInterface $request) use ($app) {
         $data = $request->getParsedBody();
         \SONFin\Models\CategoryCost::create($data);
         return $app->route('category-costs.list');
-    }, 'category-costs.store');
+    }, 'category-costs.store')
+    ->get('/category-costs/{id}/edit', function (ServerRequestInterface $request) use ($app) {
+        $view = $app->service('view.renderer');
+        $id = $request->getAttribute('id');
+        $category = \SONFin\Models\CategoryCost::findOrFail($id);
+        return $view->render('category-costs/edit.html.twig', [
+            'category' => $category
+        ]);
+    }, 'category-costs.edit')
+    ->post('/category-costs/{id}/update', function (ServerRequestInterface $request) use ($app) {
+        $id = $request->getAttribute('id');
+        $category = \SONFin\Models\CategoryCost::findOrFail($id);
+        $data = $request->getParsedBody();
+        $category->fill($data);
+        $category->save();
+        return $app->route('category-costs.list');
+    }, 'category-costs.update');
 
 $app->start();
 
