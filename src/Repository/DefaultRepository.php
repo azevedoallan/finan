02017b933720c40ba04ea4f7b332f19c4c0d8ae1
Allan Azevedo <azevedoallan3@gmail.com>
2019-08-05 23:49:55 -0300

    Iniciando com repositories

---
+++ src/Repository/DefaultRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace SONFin\Repository;
+
+
+class DefaultRepository implements RepositoryInterface
+{
+    /**
+     * @var string
+     */
+    private $modelClass;
+    /**
+     * @var Model
+     */
+    private $model;
+
+
+    /**
+     * DefaultRepository constructor.
+     * @param string $modelClass
+     */
+    public function __construct(string $modelClass)
+    {
+        $this->modelClass = $modelClass;
+        $this->model = new $modelClass;
+    }
+
+    public function all(): array
+    {
+        // TODO: Implement all() method.
+    }
+
+    public function create(array $data)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update(int $id, array $data)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete(int $id)
+    {
+        // TODO: Implement delete() method.
+    }
+}
