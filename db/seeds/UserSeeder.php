7efdd93cedd2ffe913dccfabf93f6c8ffe5f66f0
Allan Azevedo <azevedoallan3@gmail.com>
2019-08-15 23:36:07 -0300

    Implementando login e senha criptografada

--- db/seeds/UserSeeder.php
+++ db/seeds/UserSeeder.php
@@ -3,7 +3,7 @@
 
 use Phinx\Seed\AbstractSeed;
 
-class UserSeeder  extends AbstractSeed
+class UserSeeder extends AbstractSeed
 {
     /**
      * Run Method.
@@ -15,29 +15,32 @@ class UserSeeder  extends AbstractSeed
      */
     public function run()
     {
-          $faker = \Faker\Factory::create();
-          $users = $this-> table('users');
-          $users ->insert([
-              'first_name' =>$faker->firstName,
-              'last_name' => $faker->lastName,
-              'email' => 'admin@user.com',
-              'password' => '123456',
-              'created_at' => date('Y-m-d H:i:s'),
-              'updated_at' => date('Y-m-d H:i:s')
-          ])->save();
-          $data = [];
-          foreach (range(1, 3) as $value) {
-              $data[] =
-                  [
-                      'first_name' =>$faker->firstName,
-                      'last_name' => $faker->lastName,
-                      'email' => $faker->unique()->email,
-                      'password' => '123456',
-                      'created_at' => date('Y-m-d H:i:s'),
-                      'updated_at' => date('Y-m-d H:i:s')
-                  ];
-          }
+        $app = require __DIR__ . '/../bootstrap.php';
+        $auth = $app->service('auth');
 
-          $users ->insert($data)->save();
+        $faker = \Faker\Factory::create();
+        $users = $this->table('users');
+        $users->insert([
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'email' => 'admin@user.com',
+            'password' => $auth->hashPassword('123456'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ])->save();
+        $data = [];
+        foreach (range(1, 3) as $value) {
+            $data[] =
+                [
+                    'first_name' => $faker->firstName,
+                    'last_name' => $faker->lastName,
+                    'email' => $faker->unique()->email,
+                    'password' => $auth->hashPassword('123456'),
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ];
+        }
+
+        $users->insert($data)->save();
     }
 }
--- src/Plugins/AuthPlugin.php
+++ src/Plugins/AuthPlugin.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace SONFin\Plugins;
 
 use Psr\Container\ContainerInterface;
-use SON\Auth\Auth;
+use SONFin\Auth\Auth;
+use SONFin\Auth\JasnyAuth;
 use SONFin\ServiceContainerInterface;
 
 
@@ -15,8 +16,11 @@ class AuthPlugin implements PluginInterface
 
     public function register(ServiceContainerInterface $container)
     {
+        $container->addLazy('jasny.auth', function (ContainerInterface $container) {
+            return new JasnyAuth($container->get('user.repository'));
+        });
         $container->addLazy('auth', function (ContainerInterface $container) {
-            return new Auth();
+            return new Auth($container->get('jasny.auth'));
         });
     }
 
