6074488cf582fc97401d31a6056d0747f212e5ea
Allan Azevedo <azevedoallan3@gmail.com>
2019-08-26 23:51:54 -0300

    Criando estrutura de contas a receber

---
+++ db/migrations/20190827015540_create_bill_receive_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateBillReceiveTable extends AbstractMigration
+{
+    public function up()
+    {
+        $this->table('bill_receives')
+            ->addColumn('date_launch', 'date')
+            ->addColumn('name', 'string')
+            ->addColumn('value', 'float')
+            ->addColumn('user_id', 'integer')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->addForeignKey('user_id', 'users', 'id')
+            ->create();
+    }
+
+    protected function down()
+    {
+        $this->table('bill_receives')->drop()->save();
+    }
+}
---
+++ src/Models/BillReceive.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace SONFin\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class BillReceive extends Model
+{
+    //Mass Assignment
+    protected $fillable = [
+      'date_launch',
+      'name',
+      'value',
+      'user_id'
+    ];
+}
