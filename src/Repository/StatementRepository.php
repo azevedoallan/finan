2a062bff9c0892fa0a44fa688edc46bcf9a183ff
Allan Azevedo <azevedoallan3@gmail.com>
2019-09-05 01:31:52 -0300

    testando extrato

--- src/Repository/StatementRepository.php
+++ src/Repository/StatementRepository.php
@@ -6,6 +6,7 @@ namespace SONFin\Repository;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use SONFin\Models\BillPay;
+use SONFin\Models\BillReceive;
 
 class StatementRepository implements StatementRepositoryInterface
 {
@@ -14,22 +15,22 @@ class StatementRepository implements StatementRepositoryInterface
     {
         $billPays = BillPay::query()
             ->selectRaw('bill_pays.*, category_costs.name as category_name')
-            ->leftJoin('category_costs', 'category_costs.id', '=', 'bill_pays.category_costs.id')
+            ->leftJoin('category_costs', 'category_costs.id', '=', 'bill_pays.category_cost_id')
             ->whereBetween('date_launch', [$dateStart, $dateEnd])
             ->where('bill_pays.user_id', $userId)
             ->get();
 
-        $billReceives = BillPay::query()
+        $billReceives = BillReceive::query()
             ->whereBetween('date_launch', [$dateStart, $dateEnd])
             ->where('user_id', $userId)
             ->get();
         $collection = new Collection(array_merge_recursive($billPays->toArray(), $billReceives->toArray()));
-        
+
         $statements = $collection->sortByDesc('date_launch');
         return [
             'statements' => $statements,
             'total_pays' => $billPays->sum('value'),
-            'total_receives' => $billPays->sum('value')
+            'total_receives' => $billReceives->sum('value')
         ];
     }
 
--- src/controllers/statements.php
+++ src/controllers/statements.php
@@ -12,13 +12,14 @@ $app
         $dateStart = $data['date_start'] ?? (new \DateTime())->modify('-1 month');
         $dateStart = $dateStart instanceof \DateTime ? $dateStart->format('Y-m-d')
             : \DateTime::createFromFormat('d/m/Y', $dateStart)->format('Y-m-d');
+        $searchDefault = $dateStart;
 
         $dateEnd = $data['date_end'] ?? new \DateTime();
         $dateEnd = $dateEnd instanceof \DateTime ? $dateEnd->format('Y-m-d')
             : \DateTime::createFromFormat('d/m/Y', $dateEnd)->format('Y-m-d');
 
         $statements = $repository->all($dateStart,$dateEnd,$auth->user()->getId());
-
+        $statements['dateStartDefault'] = $searchDefault;
         return $view->render('statements.html.twig', [
             'statements' => $statements
         ]);
