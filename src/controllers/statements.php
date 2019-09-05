649cd5e71e8a94dd4b7b3ebbcc8fe92c86ca16b5
Allan Azevedo <azevedoallan3@gmail.com>
2019-09-05 18:41:10 -0300

    Ajuste data

--- src/controllers/statements.php
+++ src/controllers/statements.php
@@ -10,16 +10,16 @@ $app
         $auth = $app->service('auth');
         $data = $request->getQueryParams();
         $dateStart = $data['date_start'] ?? (new \DateTime())->modify('-1 month');
+        $searchDefault2 = $dateStart instanceof \DateTime ? $dateStart->format('d/m/Y') : $dateStart;
         $dateStart = $dateStart instanceof \DateTime ? $dateStart->format('Y-m-d')
             : \DateTime::createFromFormat('d/m/Y', $dateStart)->format('Y-m-d');
-        $searchDefault = $dateStart;
 
         $dateEnd = $data['date_end'] ?? new \DateTime();
         $dateEnd = $dateEnd instanceof \DateTime ? $dateEnd->format('Y-m-d')
             : \DateTime::createFromFormat('d/m/Y', $dateEnd)->format('Y-m-d');
 
-        $statements = $repository->all($dateStart,$dateEnd,$auth->user()->getId());
-        $statements['dateStartDefault'] = $searchDefault;
+        $statements = $repository->all($dateStart, $dateEnd, $auth->user()->getId());
+        $statements['dateStartDefault2'] = $searchDefault2;
         return $view->render('statements.html.twig', [
             'statements' => $statements
         ]);
